8da6142c19e0f40a3b8d887f20ac19886b8c282f
Biresh paul <palbiresh375@gmail.com>
2025-11-26 16:38:48 +0600

    added parcel cost

--- src/Pages/Sendparcel/SendParcel.jsx
+++ src/Pages/Sendparcel/SendParcel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm, useWatch } from "react-hook-form";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const SendParcel = () => {
   const {
@@ -9,9 +10,7 @@ const SendParcel = () => {
     control,
     formState: { errors },
   } = useForm();
-  const handleFormSubmit = (data) => {
-    console.log(data);
-  };
+
   const serviceCenterData = useLoaderData();
   const regionDuplicate = serviceCenterData.map((s) => s.region);
   const region = [...new Set(regionDuplicate)];
@@ -23,7 +22,45 @@ const SendParcel = () => {
     const districts = regionDistrict.map((d) => d.district);
     return districts;
   };
-
+  const handleFormSubmit = (data) => {
+    // console.log(data);
+    const isDocument = data.parcelType === "Document";
+    const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+    const parcelWeight = parseFloat(data.parcelWeight);
+    let cost = 0;
+    if (isDocument) {
+      cost = isSameDistrict ? 60 : 80;
+    } else {
+      if (parcelWeight < 3) {
+        cost = isSameDistrict ? 110 : 150;
+      } else {
+        const minCharge = isSameDistrict ? 110 : 150;
+        const extraWeight = parcelWeight - 3;
+        const extraCharge = isSameDistrict
+          ? extraWeight * 40
+          : extraWeight * 40 + 40;
+        cost = minCharge + extraCharge;
+      }
+    }
+    console.log("cost", cost);
+    Swal.fire({
+      title: "Agree with the cost",
+      text: `You will be charged ${cost} taka`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "I aggree!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+      }
+    });
+  };
   return (
     <div className="bg-white p-20 rounded-3xl mt-8">
       <h1 className="text-5xl font-bold text-secondary">Add Parcel</h1>
