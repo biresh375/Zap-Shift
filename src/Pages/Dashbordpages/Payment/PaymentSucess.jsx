b6311255b722458ae0696f7dfdbdaea6284f1828
Biresh paul <palbiresh375@gmail.com>
2025-11-30 13:15:57 +0600

    added transection id and addeed tracking id

--- src/Pages/Dashbordpages/Payment/PaymentSucess.jsx
+++ src/Pages/Dashbordpages/Payment/PaymentSucess.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router";
 import UseAxiosSecure from "../../../Hooks/UseAxiosSecure";
 
 const PaymentSucess = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const sessionId = searchParams.get("session_id");
   const axiosSecure = UseAxiosSecure();
 
@@ -13,12 +14,24 @@ const PaymentSucess = () => {
         .patch(`/session-sucess?session_id=${sessionId}`)
         .then((res) => {
           console.log(res.data);
+          setPaymentInfo({
+            transectionId: res.data.transectionId,
+            trakingId: res.data.trakingId,
+          });
         });
     }
   }, [sessionId, axiosSecure]);
   return (
     <div>
       <h1 className="text-2xl font-bold">Payment Successfull</h1>
+      <p className="font-semibold">
+        Your Transection Id :{" "}
+        <span className="font-bold ">{paymentInfo.transectionId}</span>
+      </p>
+      <p className="font-semibold">
+        Your Transection Id :{" "}
+        <span className="font-bold ">{paymentInfo.trakingId}</span>
+      </p>
       <Link className="btn btn-primary text-black" to={"/dashbord/my-parcels"}>
         Go Back
       </Link>
